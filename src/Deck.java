5882ed3e4c03c89b3ecfcd101ae586690ab4bf5d
gleb-esin <125543776+gleb-esin@users.noreply.github.com>
2023-07-08 17:12:51 +0300

    create Deck class

---
+++ src/Deck.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public class Deck {
+    private List<Card> deck;
+    private int nextCardIndex = 36-deck.size();
+
+    public Deck() {
+        SuitEnum[] suitArr = {SuitEnum.SPADES, SuitEnum.DIAMONDS, SuitEnum.CLUBS, SuitEnum.HEARTS};
+        String[] valuesArr = {"6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+        List<Card> deck = new ArrayList<>();
+        for (SuitEnum suitEnum : suitArr) {
+            for (String value : valuesArr) {
+                deck.add(new Card(value, suitEnum));
+            }
+        }
+        Collections.shuffle(deck);
+        this.deck = deck;
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer stringBuffer = new StringBuffer();
+        for (Card card: this.deck) {
+            stringBuffer.append(card + " ");
+        }
+        return stringBuffer.toString();
+    }
+
+    public int getNextCardIndex() {
+        return nextCardIndex;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
-        Card card = new Card("K", SuitEnum.HEARTS);
-        Card card2 = new Card("K",SuitEnum.CLUBS);
-
-        System.out.println(card.equals(card2));
-
-        System.out.println(card);
-        System.out.println(card2);
-        String heart = "\u2665";
-        String redHeart = "\u001B[31m" + heart + "\u001B[0m";  // ANSI-код для красного цвета и сброса цвета
-        System.out.println(redHeart);
+        Deck deck = new Deck();
+        System.out.println(deck);
 
     }
 }
