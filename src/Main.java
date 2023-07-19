5a1fa7cc85a023453e1fe3e806e55e1b035c1292
gleb-esin <125543776+gleb-esin@users.noreply.github.com>
2023-07-20 00:10:11 +0300

    fix over 6 players bug

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,12 @@ public class Main {
             System.out.println("А с кем играть-то? Повторите ввод:");
             namesArr = scanner.nextLine().split(" ");
         }
+        while (namesArr.length >6){
+            System.out.println("Максимум 6 игроков - карт-то всего 36... Повторите ввод:");
+
+            namesArr = scanner.nextLine().split(" ");
+        }
+
         Game game = new Game(namesArr);
         game.setPlayersTurn(game.getPlayers());
         Round round = new Round(game);
